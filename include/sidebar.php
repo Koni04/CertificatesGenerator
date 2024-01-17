d7f0bf1d2e53a53157ec0873feccc1f25db2e8a6
Koni04 <delgadoargie04@gmail.com>
2024-01-17 21:11:02 +0800

    Automated Certificate Generato

--- include/sidebar.php
+++ include/sidebar.php
@@ -1,14 +1,14 @@
 <!-- Sidebar -->
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
                 <!-- Sidebar - Brand -->
-                <a class="sidebar-brand" href="controlpanel.php">
-                    <div>Inventa <sup>101</sup></div>
+                <a class="sidebar-brand" href="./index.php">
+                    <div>Project <sup>101</sup></div>
                 </a>
                 <!-- Divider -->
                 <hr class="sidebar-divider my-0">
                 <!-- Nav Item - Dashboard -->
                 <li class="nav-item">
-                    <a class="nav-link" href="index.php">
+                    <a class="nav-link" href="./index.php">
                         <i class="fa-solid fa-certificate"></i>
                         <span>Certificates</span>
                     </a>
